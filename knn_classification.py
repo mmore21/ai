bdd440d0e2475aec5d880d9b3d8e0289d404e46e
Mason Moreland <mazc@protonmail.ch>
2020-01-09 23:56:16 -0800

    Create kNN file

---
+++ knn_classification.py
@@ -0,0 +1,11 @@
+"""
+File: knn_classification.py
+Author: Mason Moreland (mmore21)
+Description: k-Nearest Neighbors (kNN) classifier implemented in Python 3.
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def knn()
