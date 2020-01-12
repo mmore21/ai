2cdec96c551f3f24bae33b17ad37b4b3cd64dcb6
Mason Moreland <mazc@protonmail.ch>
2020-01-11 23:40:26 -0800

    Implement euclidean distance function

--- knn_classification.py
+++ knn_classification.py
@@ -7,5 +7,17 @@ Description: k-Nearest Neighbors (kNN) classifier implemented in Python 3.
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import math
 
-def knn()
+def knn():
+    return None
+
+def euclidian_distance(point1, point2):
+    distance = 0
+    for i in range(len(point1)):
+        distance += math.pow(point1[i] - point2[i], 2)
+    return math.sqrt(distance)
+
+if __name__ == "__main__":
+    distance = euclidian_distance([1, 2], [3, 4])
+    print(distance)
