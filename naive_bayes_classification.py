ed79c42c33eff0ae83443989d2c6a75e4c515040
Mason Moreland <mazc@protonmail.ch>
2020-02-13 23:17:29 -0800

    Start Naive Bayes classification

---
+++ naive_bayes_classification.py
@@ -0,0 +1,12 @@
+class NaiveBayesClassifier():
+    def __init__(self):
+        pass
+    
+    def fit(self, X, Y):
+        pass
+
+    def predict(self, X):
+        pass
+
+if __name__ == "__main__":
+    nb = NaiveBayesClassifier()
