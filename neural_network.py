84371e28fb8c2ff1601d392195686a2e58bfc77c
Mason Moreland <mazc@protonmail.ch>
2020-04-23 22:25:10 -0700

    Work on forward propagation and backpropagation for NN

--- neural_network.py
+++ neural_network.py
@@ -17,18 +17,41 @@ def tanh(z):
 def relu(z):
     return max(0, z)
 
+def relu_derivative(z):
+    return 1 * (z > 0)
+
 def leaky_relu(z):
     return max(0.01 * z, z)
 
 
 # --- Forward propagation
-def predict():
-    pass
+def predict(X, W, b):
+    z1 = np.dot(W, X) + b[0]
+    a1 = relu(z1)
 
-def compute_loss():
-    pass
+    z2 = np.dot(W, a1) + b[1]
+    a2 = sigmoid(z2)
 
+    A = [X, a1, a2]
+
+    return A
+
+def loss(yhat, y):
+    return -(y * np.log(yhat) + (1-y) * np.log(1-yhat))
+
+def compute_cost(yhats, ys):
+    m = len(ys)
+    J = 0
+    for i in range(m):
+        J += loss(yhats[i], ys[i])
+    return J / m
+    
 
 # --- Backpropagation
-def backpropagation():
-    pass
+def backpropagation(W, A, ys):
+    dz2 = A[2] - ys
+    dW2 = np.dot(dz2, A[1].T)
+    db2 = dz2
+    dz1 = np.dot(W[2].T, dz2) * relu_derivative(Z[1])
+    dW1 = np.dot(dz1, A[0].T)
+    db1 = dz1
