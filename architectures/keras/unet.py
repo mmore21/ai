cfc44efeb418dbf0c8aa30859190328627293c32
Mason Moreland <mazc@protonmail.ch>
2020-12-21 15:20:39 -0800

    Add template Keras code for UNet

--- architectures/keras/unet.py
+++ architectures/keras/unet.py
@@ -1,6 +1,21 @@
 import tensorflow as tf
 
+class ConvBlock(tf.Keras.Layer):
+	def __init__(self):
+		super(ConvBlock, self).__init__()
+		self.conv = 
+
 class UNet(tf.Keras.Model):
 	def __init__(self):
-		pass
+		super(UNet, self).__init__()
+		self.block_1 = UNetBlock()
+		self.block_2 = UNetBlock()
+		self.global_pool = layers.GlobalAveragePooling2D()
+		self.classifier = Dense(num_classes)
+	
+	def call(self, inputs):
+		x = self.block_1(inputs)
+		x = self.block_2(x)
+		x = self.global_pool(x)
+		return self.classifier(x)
 	
