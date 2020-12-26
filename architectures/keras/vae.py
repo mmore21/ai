0f3f4b03dbaf07e302009b4728f1c8e748a8ca9d
Mason Moreland <mazc@protonmail.ch>
2020-12-25 19:18:31 -0800

    Add blank Encoder class

--- architectures/keras/vae.py
+++ architectures/keras/vae.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 
+class Encoder(tf.keras.layers.Layer):
+	def __init__(self):
+		super(Encoder, self).__init__()
+
+
 class VariationalAutoEncoder(tf.keras.Model):
 	def __init__(self):
 		super(VariationalAutoEncoder, self).__init__()
