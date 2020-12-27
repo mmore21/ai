cc74f8c8b480ee8992c618f060a677ad58e342dc
Mason Moreland <mazc@protonmail.ch>
2020-12-26 16:33:08 -0800

    Add blank Decoder class

--- architectures/keras/vae.py
+++ architectures/keras/vae.py
@@ -3,6 +3,11 @@ import tensorflow as tf
 class Encoder(tf.keras.layers.Layer):
 	def __init__(self):
 		super(Encoder, self).__init__()
+		
+
+class Decoder(tf.keras.layers.Layer):
+	def __init__(self):
+		super(Decoder, self).__init__()
 
 
 class VariationalAutoEncoder(tf.keras.Model):
