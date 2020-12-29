942499d0ed90c569067d28849e89c9a40eb169ee
Mason Moreland <mazc@protonmail.ch>
2020-12-28 21:50:01 -0800

    Add call methods to VAE classes

--- architectures/keras/vae.py
+++ architectures/keras/vae.py
@@ -1,19 +1,28 @@
 import tensorflow as tf
 
 class Sampling(tf.keras.layers.Layer):
-	def call(self):
+	def call(self, inputs):
 		pass
 
 class Encoder(tf.keras.layers.Layer):
 	def __init__(self):
 		super(Encoder, self).__init__()
 		
+	def call(self, inputs):
+		pass
+		
 
 class Decoder(tf.keras.layers.Layer):
 	def __init__(self):
 		super(Decoder, self).__init__()
 
+	def call(self, inputs):
+		pass
+
 
 class VariationalAutoEncoder(tf.keras.Model):
 	def __init__(self):
 		super(VariationalAutoEncoder, self).__init__()
+	
+	def call(self, inputs):
+		pass
