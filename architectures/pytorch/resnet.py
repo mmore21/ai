3ec6e29931b1d4e52d515be681ec7c02ce59be3b
Mason Moreland <mazc@protonmail.ch>
2021-01-03 13:31:23 -0800

    Add blank Pytorch ResNet

---
+++ architectures/pytorch/resnet.py
@@ -0,0 +1,17 @@
+import torch
+
+class ResNetBlock(torch.nn.Module):
+	def __init__(self):
+		super(ResNetBlock, self).__init__()
+	
+	
+	def forward(self, inputs):
+		pass
+
+
+class ResNet(torch.nn.Module):
+	def __init__(self):
+		super(ResNetBlock, self).__init__()
+	
+	def foward(self, inputs):
+		pass
